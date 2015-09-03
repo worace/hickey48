24225f2d76ecd1e5c35d79216985f23cad0e420a
worace <horace.d.williams@gmail.com>
2015-09-02 22:07:35 -0600

    moving figwheel to dev dep

--- project.clj
+++ project.clj
@@ -7,11 +7,12 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-3211"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [figwheel "0.2.7-SNAPSHOT"]
                  [prismatic/dommy "1.1.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [reagent "0.5.0-alpha3"]
                  ]
+
+  :profiles {:dev {:dependencies [[figwheel "0.2.7-SNAPSHOT"]]}}
 
   :plugins [[lein-cljsbuild "1.0.5"]
             [lein-figwheel "0.3.3"]
